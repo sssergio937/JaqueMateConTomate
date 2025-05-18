bbe9ef6efb650fc442bd62978c55e993933bb276
oscarvaquero4 <oscar.vaquero@alumnos.upm.es>
2025-05-18 17:31:23 +0200

    Delete src directory

--- src/casilla.cpp
+++
@@ -1,6 +0,0 @@
-#include "casilla.h"
-
-// Devuelve true si el movimiento entre esta casilla y otra NO es diagonal
-bool Casilla::noSaltarDiagonal(const Casilla& inicio, const Casilla& final) const {
-    //lo que sea q haga esta funcion
-}
--- src/casilla.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-class Casilla
-{
-public:
-    int fila, columna;
-    bool noSaltarDiagonal(const Casilla& inicio, const Casilla& final) const;
-};
-
