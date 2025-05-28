56932cdbf4d7258e98f3f5fb3ceaf586474ff872
Diego Manzano <127451491+diegomanzanoggg@users.noreply.github.com>
2025-05-28 13:36:43 +0200

    Delete JaqueMateConTomate-cambios directory

--- JaqueMateConTomate-cambios/src/rey.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "pieza.h"
-class Rey : public Pieza
-{
-public:
-	Rey(COLOR c, TIPO t, const char* im_b, const char* im_n) : Pieza(c, t, im_b, im_n) {}
-	virtual bool movimiento_valido(Casilla inicio, Casilla fin, Tablero& celda);
-};
-
