a5641d1d5162df40e2de1923f34d3368dc94f249
MP-19-creator <xmario0425@gmail.com>
2025-05-29 18:12:59 +0200

    Add files via upload

---
+++ JaqueMateConTomate (Movmiento_Piezas)+JAQUE Y JAQUEMATE y coronacion de peones y reinicio de partida/src/rey.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "pieza.h"
+class rey : public Pieza
+{
+public:
+	rey(COLOR color) : Pieza(color, TIPO::REY, "imagenes/rey rojo.png", "imagenes/rey verde.png") {};
+	bool movimiento_valido(Casilla inicio, Casilla fin, Tablero& tablero);
+};
+
---
+++ JaqueMateConTomate (Movmiento_Piezas)+JAQUE Y JAQUEMATE y coronacion de peones y reinicio de partida/src/torre.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "pieza.h"
+class torre : public Pieza
+{
+public:
+	torre(COLOR color) : Pieza(color, TIPO::TORRE, "imagenes/torre roja.png", "imagenes/torre verde.png") {};
+	bool movimiento_valido(Casilla inicio, Casilla fin, Tablero& tablero);
+};
+
